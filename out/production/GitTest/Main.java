066ecde8271242c677e52c6dda1dbb84afb90f1f
Saidul <saidul@duck.com>
2024-04-01 02:06:18 +0530

    featureBranch deleted

---
+++ out/production/GitTest/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello from intellij Idea, an IDE developed by Jetbrains");
+        System.out.println(isOdd(44));
+    }
+    public static boolean isOdd(int number){
+        return (number%2 == 1);
+    }
+}
