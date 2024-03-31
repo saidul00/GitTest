470166191d4a897b85f8908962487dbfadcd3bb6
Saidul <saidul@duck.com>
2024-04-01 02:27:26 +0530

    REMOVED FILES

--- out/production/GitTest/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello from intellij Idea, an IDE developed by Jetbrains");
-        System.out.println(isOdd(44));
-    }
-    public static boolean isOdd(int number){
-        return (number%2 == 1);
-    }
-}
